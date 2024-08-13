6beafbcf75977b403e2d269f068600c62764f761
Nicholas Chen <nichwch@gmail.com>
2024-08-13 13:03:16 -0400

    validator logic

--- validator/main.py
+++ validator/main.py
@@ -1,4 +1,5 @@
 from typing import Any, Callable, Dict, Optional
+from fuzzysearch import find_near_matches
 
 from guardrails.validator_base import (
     FailResult,
@@ -6,6 +7,7 @@ from guardrails.validator_base import (
     ValidationResult,
     Validator,
     register_validator,
+    ErrorSpan
 )
 
 
@@ -23,30 +25,43 @@ class BanList(Validator):
 
     Args:
         banned_words (List[str]): A list of banned words to check for in output.
+        max_l_dist (int): Maximum Levenshtein distance for fuzzy search. Default is 1.
     """  # noqa
 
     # If you don't have any init args, you can omit the __init__ method.
     def __init__(
         self,
         banned_words: str,
+        max_l_dist: int = 1,
         on_fail: Optional[Callable] = None,
     ):
         super().__init__(on_fail=on_fail, arg_1=banned_words)
         self._banned_words = banned_words
+        self._max_l_dist = max_l_dist
 
     def validate(self, value: Any, metadata: Dict = {}) -> ValidationResult:
         """Validates that output does not have banned words."""
         # Add your custom validator logic here and return a PassResult or FailResult accordingly.
+        all_matches = []
         for banned_word in self._banned_words:
-            if banned_word in value:
-                return FailResult(
-                    error_message=f"Output contains banned word: {banned_word}",
-                    fix_value=value.replace(banned_word, ""),
-                ) 
-
-        if value != "pass": # FIXME
+            matches = find_near_matches(banned_word, value, max_l_dist=self._max_l_dist)
+            all_matches.extend(matches)
+        
+        if len(all_matches) > 0:
+            error_spans = []
+            fix_value = value
+            for match in all_matches:
+                triggering_text = value[match.start:match.end]
+                fix_value = fix_value.replace(triggering_text, "")
+                error_spans.append(ErrorSpan(
+                    start=match.start,
+                    end=match.end,
+                    reason=f"Found match with banned word '{match.matched}' in '{triggering_text}'"
+                ))
             return FailResult(
-                error_message="{A descriptive but concise error message about why validation failed}",
-                fix_value="{The programmtic fix if applicable, otherwise remove this kwarg.}",
+                error_message="Output contains banned words",
+                error_spans=error_spans,
+                fix_value=fix_value
             )
+
         return PassResult()
